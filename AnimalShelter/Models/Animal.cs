f9f8f7e18d28984b4f652748b007c345847325d2
Joey Palchak <joey.palchak@gmail.com>
2023-10-03 15:43:01 -0700

    create Animal Model and AnimalShelterContex
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,14 @@
+namespace AnimalShelter.Models
+{
+
+  public class Animal
+  {
+    public int Id { get; set; }
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Breed { get; set; }
+    public string DateOfAdmittance { get; set; }
+  }
+
+} 
+
